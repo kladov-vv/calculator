969d60fe528655543f70d2777b54c6e5fc460720
kladov-vv <kladov.vv@yandex.com>
2024-12-14 14:39:51 +1000

    Add operate function that calls basic math functions

--- main.js
+++ main.js
@@ -2,6 +2,19 @@ let firstNumber;
 let secondNumber;
 let operator;
 
+function operate(firstNumber, secondNumber, operator){
+    switch (operator) {
+        case add:
+            return add(firstNumber, secondNumber);
+        case subtract:
+            return subtract(firstNumber, secondNumber);
+        case multiply:
+            return multiply(firstNumber, secondNumber);
+        case divide:
+            return divide(firstNumber, secondNumber);
+    }
+}
+
 function add(a, b) {
     return a + b;
 }
@@ -17,4 +30,3 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b;
 }
-
