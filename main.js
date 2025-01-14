a3f09844f6dd576503179024023cf39e0b174b73
kladov-vv <kladov.vv@yandex.com>
2025-01-14 19:11:50 +1000

    Add basic calculator finctionality

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
-let firstNumber;
-let secondNumber;
-let operator;
+let firstNumber = '';
+let secondNumber = '';
+let operator = '';
+let currentNumber = '';
 const display = document.querySelector('.display');
 const keyboard = document.querySelector('.keyboard');
 
@@ -8,20 +9,63 @@ keyboard.addEventListener('click', keyboardInput);
 
 function keyboardInput(event) {
     let target = event.target;
-    if (target.tagName === 'BUTTON') {
-        console.log(target.parentElement);
+    switch (target.parentElement.className) {
+        case 'numbers':
+            console.log('number');
+            populateDisplay(target.textContent);
+            break;
+        case 'operators':
+            console.log('operator');
+            if (firstNumber === '' && secondNumber === '') {
+                operator = target.textContent;
+                firstNumber = currentNumber;
+                currentNumber = '';
+            }
+            if (firstNumber != '') {
+                secondNumber = currentNumber;
+                
+                currentNumber = operate(firstNumber, secondNumber, operator);
+                display.textContent = currentNumber;
+
+                firstNumber = currentNumber;
+                secondNumber = '';
+                operator = target.textContent;
+                currentNumber = '';
+            }
+            break;
+        case 'equal':
+            console.log('equal');
+            if (firstNumber != '') {
+                secondNumber = currentNumber;
+                
+                currentNumber = operate(firstNumber, secondNumber, operator);
+                display.textContent = currentNumber;
+
+                firstNumber = currentNumber;
+                secondNumber = '';
+                operator = '';
+                currentNumber = '';
+            }
+            break;
     }
 }
 
+function populateDisplay(number) {
+    currentNumber += number;
+    display.textContent = currentNumber;
+}
+
 function operate(firstNumber, secondNumber, operator){
+    firstNumber = Number(firstNumber);
+    secondNumber = Number(secondNumber);
     switch (operator) {
-        case 'add':
+        case '+':
             return add(firstNumber, secondNumber);
-        case 'subtract':
+        case '-':
             return subtract(firstNumber, secondNumber);
-        case 'multiply':
+        case '*':
             return multiply(firstNumber, secondNumber);
-        case 'divide':
+        case '/':
             return divide(firstNumber, secondNumber);
     }
 }
